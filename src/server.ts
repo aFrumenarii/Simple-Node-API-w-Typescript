845700126e4b7b7abf011ed72e1179af9618f587
aFrumenarii <silvasousalucas@protonmail.com>
2023-06-01 13:27:35 -0300

    second commit

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get("/", (req, res) => {
+    return res.json({message: "This is a message from the Back-end!"})
+})
+
 app.listen(3333, () => {
-    console.log("The server is listenin on port 3333")
+    console.log("The server is listening on port 3333")
 })
