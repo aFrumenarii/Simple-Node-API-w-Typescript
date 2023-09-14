69332e0ebaa0748ba963455be99301f9bed5e3d5
Lucas Sousa da Silva <106290013+aFrumentarii@users.noreply.github.com>
2023-09-14 07:15:17 -0300

    Atualizar o CreateSpecificationService.ts

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -6,7 +6,7 @@ interface IRequest {
 }
 
 class CreateSpecificationService {
-  constructor(private specificationsRepository: SpecificationsRepository) {}
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists =
